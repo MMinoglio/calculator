bf3ea4e98e671dcdef5d653b0c8ba74c0114d340
MMinoglio <martin.minoglio@outlook.com>
2021-12-30 12:15:42 -0300

    primer diseño html y css finalizado

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  result = 0;
+  switch (operator) {
+    case "+":
+      result = add (a, b);
+      break;
+    case "-":
+      result = substract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+  }
+  return result;
+}
+
